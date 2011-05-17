b62cda6516ae7f3307f122db9c211f0936b58faf
nikola chochkov <nikola@howkul.info>
2011-05-17 20:25:20 +0200

    benchmark test extension

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -2,39 +2,42 @@
 require File.join(File.dirname(__FILE__), '..', 'spec', 'tester')
 include GreenMidget
 
-REPETITIONS        = 20
-MESSAGE_LENGTH     = 1000
+REPETITIONS             = 20
+MESSAGE_LENGTH          = 1000
 
-@train_alternative = []
-@train_null        = []
-@no_caching        = []
-@caching           = []
+@train_alternative      = []
+@train_null             = []
+@known_words            = []
+@unknown_words          = []
+@unknown_words_fetch    = []
+@known_words_fetch      = []
+
+records_count_at_start  = GreenMidgetRecords.count
 
 REPETITIONS.times do
   a = Tester.new_with_random_text(MESSAGE_LENGTH)
 
-  @train_alternative << Benchmark.measure{ a.classify_as! CATEGORIES.last }.real
-  @train_null        << Benchmark.measure{ a.classify_as! CATEGORIES.first }.real
+  @train_alternative   << Benchmark.measure{ a.classify_as! CATEGORIES.last }.real
+  @train_null          << Benchmark.measure{ a.classify_as! CATEGORIES.first }.real
+  @known_words         << Benchmark.measure{ a.classify }.real
+  @known_words_fetch   << Benchmark.measure{ GreenMidgetRecords.fetch_all(a.words) }.real
 
-  GreenMidgetRecords.class_eval do
-    def self.[](key)
-      find_by_key(key)
-    end
-  end
-  @no_caching << Benchmark.measure{ a.classify }.real
+  b = Tester.new_with_random_text(MESSAGE_LENGTH)
 
-  GreenMidgetRecords.class_eval do
-    def self.[](key)
-      key = key.to_s
-      @@cache ||= {}
-      @@cache[key] || @@cache[key] = find_by_key(key) || @@cache[key] = new(key)
-    end
-  end
-  @caching    << Benchmark.measure{ a.classify }.real
+  @unknown_words       << Benchmark.measure{ b.classify }.real
+  @unknown_words_fetch << Benchmark.measure{ GreenMidgetRecords.fetch_all(b.words) }.real
 end
 
-puts "Average times from #{ REPETITIONS } repetitions and #{ MESSAGE_LENGTH } words per message"
-puts "Examples in the alternative hypothesis: #{ @train_alternative.sum.to_f/REPETITIONS  }"
-puts "Examples in the null hypothesis: #{        @train_null.sum.to_f/REPETITIONS         }"
-puts "Classification without caching: #{         @no_caching.sum.to_f/REPETITIONS         }"
-puts "Classification with caching: #{            @caching.sum.to_f/REPETITIONS            }"
+puts "-------------------------------"
+puts "Average times in seconds from #{ REPETITIONS } repetitions and #{ MESSAGE_LENGTH } words per message:"
+puts "Number of records at start: #{ records_count_at_start } and at the end: #{ GreenMidgetRecords.count }"
+puts "-------------------------------"
+puts "Training unknown words:                 #{ (@train_alternative.sum.to_f/REPETITIONS).round(4) }"
+puts "Training known words:                   #{ (@train_null.sum.to_f/REPETITIONS).round(4) }"
+puts "-------------------------------"
+puts "Classification of known words:          #{ (@known_words.sum.to_f/REPETITIONS).round(4) }"
+puts "Data fetch of known words:              #{ (@known_words_fetch.sum.to_f/REPETITIONS).round(4) }"
+puts "-------------------------------"
+puts "Classification of unknown words:        #{ (@unknown_words.sum.to_f/REPETITIONS).round(4) }"
+puts "Data fetch of known words:              #{ (@unknown_words_fetch.sum.to_f/REPETITIONS).round(4) }"
+puts "-------------------------------"
